d6205d2945c4d3ba3b215e9ac03de14e0c0a500a
DerLed <48630545+DerLed@users.noreply.github.com>
2021-11-09 16:05:19 +0300

    Update dictionary_template.py

---
+++ dictionary_template.py
@@ -0,0 +1,21 @@
+dictionary_template = {
+    'thickness': 4,
+    'metal_grade': 10,  # Марка металла
+    'gost': 11,  # Гост материала
+    'melting_number': 12,  # Номер плавки
+    'batch_number': 12,  # Номер партии
+    'certificate_number': 13,  # Номер сертификата
+    'date_of_certificate': 13,  # Дата сертификата
+    'limit_fluidity': 14,  # Предел текучести
+    'temporary_resistance': 15,  # Временное сопротивление
+    'relative_extension': 16,  # Относительное удлинение
+    'relative_narrowing': 17,  # Относительное сужение
+    'impact_strength_before_aging': 18,  # Ударная вязкость до старения
+    'impact_strength_after_aging': 19,  # Ударная вязкость после старения
+    'sample_type': 20,  # Тип образца
+    'impact_strength_below_zero': 21,  # Ударная вязкость ниже ноля
+    'temperature_below_zero': 22,  # Температура ниже ноля
+    'sample_type_below_zero': 23,  # Тип образка ниже ноля
+    'additional_data': 24  # Доболнительные данные
+    # chemical_composition = []  # Хим.состав
+}
