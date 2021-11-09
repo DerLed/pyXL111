89b46c7d4f3c45cd3470cd15bee59599c5f6a2ad
DerLed <48630545+DerLed@users.noreply.github.com>
2021-11-09 23:15:40 +0300

    2

--- mydb.py
+++ mydb.py
@@ -1,31 +1,27 @@
 import sqlite3
 
 
-conn = sqlite3.connect('orders.db')
+conn = sqlite3.connect('cert_db.db')
 cur = conn.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS users(
-   userid INT PRIMARY KEY,
-   fname TEXT,
-   lname TEXT,
-   gender TEXT);
+cur.execute("""CREATE TABLE IF NOT EXISTS cert_db(
+   cert_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+   certificate_number TEXT,
+   thickness TEXT);
 """)
 conn.commit()
 
-customers = [
-  ('00005', 'Stephanie', 'Stewart', 'female'), ('00006', 'Sincere', 'Sherman', 'female'), ('00007', 'Sidney', 'Horn', 'male'),
-  ('00008', 'Litzy', 'Yates', 'female'), ('00009', 'Jaxon', 'Mills', 'male'), ('00010', 'Paul', 'Richard', 'male'),
-  ('00011', 'Kamari', 'Holden', 'female'), ('00012', 'Gaige', 'Summers', 'female'), ('00013', 'Andrea', 'Snow', 'female'),
-  ('00014', 'Angelica', 'Barnes', 'female'), ('00015', 'Leah', 'Pitts', 'female'), ('00016', 'Dillan', 'Olsen', 'male'),
-  ('00017', 'Joe', 'Walsh', 'male'), ('00018', 'Reagan', 'Cooper', 'male'), ('00019', 'Aubree', 'Hogan', 'female'),
-  ('00020', 'Avery', 'Floyd1111', 'male'), ('00021', 'Elianna', 'Simmons', 'female'), ('00022', 'Rodney', 'Stout', 'male'),
-  ('00023', 'Elaine', 'Mcintosh', 'female'), ('00024', 'Myla', 'Mckenzie', 'female'), ('00025', 'Alijah', 'Horn', 'female'),
-  ('00026', 'Rohan', 'Peterson', 'male'), ('00027', 'Irene', 'Walters', 'female'), ('00028', 'Lilia', 'Sellers', 'female'),
-  ('00029', 'Perla1', 'Jefferson111', 'female'), ('00030', 'Ashley', 'Klein', 'female')
+cert = [
+  ('1458-18785', 'лист 8х1500х6000'),
+  ('14545-456456', 'лист 12х1500х6000'),
+  ('4568-1546', 'лист 16х1500х6000'),
+  ('888-456456', 'лист 12х1500х6000')
 ]
 
-cur.executemany("INSERT INTO users VALUES(?, ?, ?, ?);", customers)
+cur.execute("""INSERT INTO cert_db VALUES('0001', '1458-18785', 'лист 12х1500х6000');""")
+cur.execute("""INSERT INTO cert_db VALUES('0002', '888-18785', 'лист 16х1500х6000');""")
+cur.execute("""INSERT INTO cert_db VALUES('0003', '888-18785', 'лист 16х1500х6000');""")
 
-cur.execute("SELECT * FROM users WHERE gender=?;", [('male')])
+cur.execute("SELECT rowid, cert_id FROM cert_db GROUP BY cert_id;")
 three_results = cur.fetchall()
 print(three_results)
